e9eca163f557b6938831746b5bb65d59d982e021
Derek Murray <derekmurray@Dereks-iMac.local>
2021-03-22 17:18:34 -0400

    added carosel and images to homepage, added bootstrap link to index.html

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import SneakerCard from './SneakerCard';
 
-function HomePage(props){
-    return(
-        <div className = 'HomePage'>
-            <h1 className = 'HomePage-title'>Home Page</h1>
-            <div className = 'HomePage-cards'>
-                {/* {props.sneakers.map(s => (
-                    <Sneakercard
-                        brand = {s.id}
-                        colorway = {s.colorway}
-                        name = {s.name}
-                        releaseDate = {s.releaseDate}
-                        retailPrice = {s.retailPrice}
-                        shoe = {s.shoe}
-                        title = {s.title}
-                        year = {s.year}
-                    />
-                ))} */}
-            </div>
-        </div>
+function HomePage(props) {
+    return (
+        <div className='HomePage'>
+            <h1 className='HomePage-title'>Home Page</h1>
+            <div className='HomePage-cards'>
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://images.stockx.com/images/Nike-Court-Borough-Low-2-Black-University-Red-GS.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1614886339" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://images.stockx.com/images/Nike-Dunk-High-Game-Royal.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1613038501" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://images.stockx.com/images/Nike-Dunk-Low-College-Navy-Grey-W-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1615920429" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+                        </div>
+                    </div>
     );
 }
 
--- src/NikePage.js
+++ src/NikePage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import SneakerCard from './SneakerCard';
 import SneakerCardList from './SneakerCardList';
 
-function NikePage(){
+function NikePage() {
     console.debug('NikePage');
     const url = "https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=nike";
 
@@ -18,39 +18,39 @@ function NikePage(){
         });
     }, [url])
 
-    if(sneakers){
+    if (sneakers) {
         content =
-        <div>
             <div>
-                <h1>{sneakers[0].brand}</h1>
+                <div>
+                    <h1>{sneakers[0].brand}</h1>
+                </div>
+                <div>
+                    <h1>{sneakers[0].name}</h1>
+                </div>
+                <div>
+                    <h1>{sneakers[0].retailPrice}</h1>
+                </div>
+                <div>
+                    <img src={sneakers[0].media.smallImageUrl} />
+                </div>
+                <div>
+                    <SneakerCardList sneakers={sneakers} />
+                </div>
             </div>
-            <div>
-                <h1>{sneakers[0].name}</h1>
-            </div>
-            <div>
-                <h1>{sneakers[0].retailPrice}</h1>
-            </div>
-            <div>
-                <img src={sneakers[0].media.smallImageUrl}/>
-            </div>
-            <div>
-            <SneakerCardList sneakers={sneakers} />
-        </div>
-        </div>
     }
 
-    else{
+    else {
         <h1>Loading...</h1>
     }
 
-    return(
-        <div className = 'NikePage'>
-            <h1 className = 'NikePage-title'>Nike Page</h1>
-            <h2 className = 'NikePage-title'>Displays SneakerCards for Nike shoes</h2>
-            <div className = 'NikePage-cards'>
-            <div>
-                {content}
-            </div>
+    return (
+        <div className='NikePage'>
+            <h1 className='NikePage-title'>Nike Page</h1>
+            <h2 className='NikePage-title'>Displays SneakerCards for Nike shoes</h2>
+            <div className='NikePage-cards'>
+                <div>
+                    {content}
+                </div>
             </div>
         </div>
     );
