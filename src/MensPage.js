e3e56ab257eb8892c7d46a32a42f67917a99d38a
nbry <nwbryant@umich.edu>
2022-10-28 09:04:45 -0700

    refactor(MensPage.js): run linter/remove all unused

--- src/MensPage.js
+++ src/MensPage.js
@@ -1,78 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Card, Button } from "react-bootstrap";
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
+function MensPage() {
+    const [sneakers, setSneakers] = useState(null);
 
-function MensPage(props){
+    const mens = {
+        method: "GET",
+        url: "https://v1-sneakers.p.rapidapi.com/v1/sneakers",
+        params: { limit: "100", gender: "men" },
+        headers: {
+            "x-rapidapi-key":
+                "d35e6f2cf6msh582d393a4408760p1fd4ddjsna38953b14404",
+            "x-rapidapi-host": "v1-sneakers.p.rapidapi.com",
+        },
+    };
 
-        const [sneakers, setSneakers] = useState(null);
-
-        const mens = {
-          method: 'GET',
-          url: 'https://v1-sneakers.p.rapidapi.com/v1/sneakers',
-          params: { limit: '100', gender: "men" },
-          headers: {
-            'x-rapidapi-key': 'd35e6f2cf6msh582d393a4408760p1fd4ddjsna38953b14404',
-            'x-rapidapi-host': 'v1-sneakers.p.rapidapi.com'
-          }
-        };
-
-        useEffect(() => {
-          async function getSneakers(){
-            try{
-              const response = await axios.get(mens.url, mens);
-              const { results } = response.data;
-              setSneakers(results.filter(s => {
-                const { smallImageUrl } = s.media;
-                return smallImageUrl && smallImageUrl !== '';
-              }))
+    useEffect(() => {
+        async function getSneakers() {
+            try {
+                const response = await axios.get(mens.url, mens);
+                const { results } = response.data;
+                setSneakers(
+                    results.filter(s => {
+                        const { smallImageUrl } = s.media;
+                        return smallImageUrl && smallImageUrl !== "";
+                    }),
+                );
+            } catch (e) {
+                console.log("Theres an error somewhere");
             }
-            catch(e){
-              console.log('Theres an error somewhere');
-            }
-          };
-          getSneakers();
+        }
+        getSneakers();
+    });
 
-        })
-
-        function SneakerDisplay() {
-            return (
-              <div className="row justify-content-center ">
+    function SneakerDisplay() {
+        return (
+            <div className="row justify-content-center ">
                 {sneakers
-                  ? sneakers.map((s, idx) => (
-                      <Card
-                        key={s.id}
-                        className="mr-2"
-                        title={s.title}
-                        brand={s.brand}
-                        colorway={s.colorway}
-                        style={{ width: "18rem" }}
-                        shoe={s.shoe}
-                        name={s.name}>
-                        <Card.Body>
-                          <Card.Img variant="top" src={s.media.smallImageUrl} />
-                          <Card.Title>{s.title}</Card.Title>
-                          <Card.Text>
-                                    <div>{s.colorway}</div>
-                                    <div>Release Date: {s.releaseDate}</div>
-                                    <div>Retail Price: ${s.retailPrice}</div>
-                          </Card.Text>
-                          <Button variant="primary">Save</Button>
-                        </Card.Body>
-                      </Card>
-                    ))
-                  : null}
-              </div>
-            );
-          }
+                    ? sneakers.map(s => (
+                          <Card
+                              key={s.id}
+                              className="mr-2"
+                              title={s.title}
+                              brand={s.brand}
+                              colorway={s.colorway}
+                              style={{ width: "18rem" }}
+                              shoe={s.shoe}
+                              name={s.name}
+                          >
+                              <Card.Body>
+                                  <Card.Img
+                                      variant="top"
+                                      src={s.media.smallImageUrl}
+                                  />
+                                  <Card.Title>{s.title}</Card.Title>
+                                  <Card.Text>
+                                      <div>{s.colorway}</div>
+                                      <div>Release Date: {s.releaseDate}</div>
+                                      <div>Retail Price: ${s.retailPrice}</div>
+                                  </Card.Text>
+                                  <Button variant="primary">Save</Button>
+                              </Card.Body>
+                          </Card>
+                      ))
+                    : null}
+            </div>
+        );
+    }
 
-    return(
-        <div className = 'FrontPage'>
+    return (
+        <div className="FrontPage">
             <Jumbotron fluid>
                 <Container>
-                    <h1>Men's</h1>
+                    <h1>{"Men's"}</h1>
                 </Container>
             </Jumbotron>
             <div>
